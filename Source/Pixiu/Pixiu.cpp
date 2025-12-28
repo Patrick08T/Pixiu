f4fc0956e4dd11f56c2f01cd072049ce11586e73
patrick08T <patrick08t@qq.com>
2025-12-28 18:31:58 +0800

    init

---
+++ Source/Pixiu/Pixiu.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Pixiu.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Pixiu, "Pixiu" );
