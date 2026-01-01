f46eb5310feae23daa67128c32b5289b41c2bd40
patrick08T <patrick08t@qq.com>
2026-01-01 21:27:45 +0800

    2-11 动画蓝图+自定义动画蓝图类

---
+++ Source/Pixiu/Private/Animations/CAnimInstance.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animations/CAnimInstance.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UCAnimInstance::NativeInitializeAnimation()
+{
+	OwnerCharacter = Cast<ACharacter>(TryGetPawnOwner());
+	if (OwnerCharacter)
+	{
+		OwnerMovementComp = OwnerCharacter->GetCharacterMovement();
+	}
+}
+
+void UCAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+
+}
+
+void UCAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+    // Intentionally left empty; thread-safe per-frame update can be implemented here.
+}
